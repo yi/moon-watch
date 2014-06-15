a8845ca632ba87a2acdc07490b15e0674c35f380
yi <yi2004@gmail.com>
2014-06-16 02:43:26 +0800

    complete moon-watch script

--- moon-watch.sh
+++ moon-watch.sh
@@ -2,32 +2,25 @@
 
 
 TARGET_DIR=$2
-if [ -z "$TARGET_DIR" ]
-then
-	TARGET_DIR="`dirname "$0"`"
-fi
+if [ -z "$TARGET_DIR" ]; then TARGET_DIR="`dirname "$0"`"; fi
 TARGET_DIR=$(cd "$TARGET_DIR" && pwd)
 
-#echo "TARGET_DIR: $TARGET_DIR"
-
 SRC_DIR=$1
-if [ -z "$SRC_DIR" ]
-then
-	SRC_DIR="`dirname "$0"`"
-fi
+if [ -z "$SRC_DIR" ]; then SRC_DIR="`dirname "$0"`"; fi
 SRC_DIR=$(cd "$SRC_DIR" && pwd)
-
-#echo "SRC_DIR: $SRC_DIR"
 
 echo "moon-watch start: $SRC_DIR ==> $TARGET_DIR"
 
 fsw -0 $SRC_DIR | while read -d "" i
 do
   if echo $i | grep -q "^.*moon$" > /dev/null ; then
-    part=$(echo $i | sed "s|$SRC_DIR|$EMPTY|g")
-    dest="`dirname $TARGET_DIR$part`"
+    PART=$(echo $i | sed "s|$SRC_DIR|$EMPTY|g")
+    DEST="`dirname $TARGET_DIR$PART`"
+    RESULT_FILENAME=$(echo "`basename $i`" | sed "s/\.moon/\.lua/g")
     cd "`dirname $i`"
-    moonc -t $dest "`basename $i`" && echo "==> $dest/"
+    echo "dest: $DEST"
+    moonc -t $DEST "`basename $i`" && echo "=> $DEST/$RESULT_FILENAME (`date`)"
+    if [ $? != 0 ]; then tput bel; fi  # beep when compile failed
   fi
 done
 
